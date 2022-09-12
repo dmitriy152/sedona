adb325cb01f04a5150d5b030984d7180bc4c8aa7
Дмитрий <93095763+dmitriy152@users.noreply.github.com>
2022-09-12 09:52:46 +0300

    dmitriy152-patch-1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { popup } from './popup.js';
+import { buttonLike } from './buttonLikeCounter.js'
+import { searchHotel } from './searchHotel.js'
+import { videoCatalog } from './videoCatalog.js'
+searchHotel();
+buttonLike();
+popup();
+videoCatalog();
