744f0ee151790307cc24339af9a33a6e5a9d778d
Дмитрий <93095763+dmitriy152@users.noreply.github.com>
2022-09-16 13:24:25 +0300

    form

---
+++ js/formButtonSent.js
@@ -0,0 +1,20 @@
+function formButtonSent(){
+    if (document.querySelector(".successfully_sent")){
+        let formBlock =  document.querySelector(".successfully_sent")
+        let submiteForm = document.querySelector(".submite_form")
+        
+        submiteForm.addEventListener("click", function(evt){
+            evt.preventDefault()
+            formBlock.classList.toggle("closed_form")
+            formBlock.scrollIntoView({block: "center"});
+        })
+
+        
+        let buttonSend = document.querySelector(".form_button_send")
+        buttonSend.addEventListener("click", function(){
+            formBlock.classList.toggle("closed_form")
+        })
+    }
+}
+
+export {formButtonSent};
