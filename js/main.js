1479ffa92a1c0b7cd283a7a818457c228fe3d1ca
Дмитрий <93095763+dmitriy152@users.noreply.github.com>
2022-10-07 15:49:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { popup } from './popup.js';
+import { buttonLike } from './buttonLikeCounter.js'
+import { searchHotel } from './searchHotel.js'
+import { videoCatalog } from './videoCatalog.js'
+import { formButtonSent } from './formButtonSent.js'
+searchHotel();
+buttonLike();
+popup();
+videoCatalog();
+formButtonSent();
